00b2afbd3466b552d3aa5367d7f691c32cb882d6
kavin raj <sikavinraj@gmail.com>
2025-07-01 13:59:26 +0530

    Enhance product page with improved image gallery and styling
    - Updated photoswipe-control.js to collect images from thumbnails and handle click events for large images.
    - Added favicon link in theme.liquid for better branding.
    - Refactored main-product.liquid to improve layout and structure of product details and images.
    - Introduced new styles in _product.scss for better visual presentation of product elements.
    - Adjusted base.scss to import product styles.

--- assets/photoswipe-control.js
+++ assets/photoswipe-control.js
@@ -1,3 +1,18 @@
-$(document).ready(function () {
 
-});
+  $(document).ready(function () {
+    // Step 1: Collect all images from thumbnails
+    var pswpItems = [];
+
+    $('#gallery_01 a').each(function () {
+      var $link = $(this);
+      var src = $link.data('zoom-image');
+      var msrc = $link.find('img').attr('src');
+    });
+
+    // Step 2: On click of large image
+    $('.test-gallery').on('click', function (e) {
+      e.preventDefault();
+
+      var currentSrc = $(this).attr('data-zoom-image');
+    });
+  });
