18fedfda467253c9ad076724f6bc4bfedcc50748
kavin raj <sikavinraj@gmail.com>
2025-07-07 10:56:07 +0530

    Refactor and update various sections and styles
    - Commented out unused code in photoswipe-control.js for better readability.
    - Updated image URL filter from `img_url` to `image_url` in about-us.liquid, product-recommendations.liquid, and social-meta-tags.liquid for consistency.
    - Changed heading classes from `page-title` to `heading--1` in customer-login.liquid, customer-register.liquid, customer-reset-password.liquid, main-cart.liquid, and search-results.liquid for uniformity in styling.
    - Enhanced the product variant selection logic in main-product.liquid to handle multiple variants more effectively.
    - Removed deprecated styles from base.scss and added new styles in _login.scss and _product.scss for improved layout and design.
    - Added custom pagination controls in search-results.liquid for better navigation experience.

--- assets/photoswipe-control.js
+++ assets/photoswipe-control.js
@@ -1,18 +1,18 @@
 
-  $(document).ready(function () {
-    // Step 1: Collect all images from thumbnails
-    var pswpItems = [];
+  // $(document).ready(function () {
+  //   // Step 1: Collect all images from thumbnails
+  //   var pswpItems = [];
 
-    $('#gallery_01 a').each(function () {
-      var $link = $(this);
-      var src = $link.data('zoom-image');
-      var msrc = $link.find('img').attr('src');
-    });
+  //   $('#gallery_01 a').each(function () {
+  //     var $link = $(this);
+  //     var src = $link.data('zoom-image');
+  //     var msrc = $link.find('img').attr('src');
+  //   });
 
-    // Step 2: On click of large image
-    $('.test-gallery').on('click', function (e) {
-      e.preventDefault();
+  //   // Step 2: On click of large image
+  //   $('.test-gallery').on('click', function (e) {
+  //     e.preventDefault();
 
-      var currentSrc = $(this).attr('data-zoom-image');
-    });
-  });
+  //     var currentSrc = $(this).attr('data-zoom-image');
+  //   });
+  // });
